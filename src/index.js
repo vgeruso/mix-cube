b8d82cdcc11c20146f9e05009515ee256d3a3579
VictorGeruso <victor.geruso@gmail.com>
2019-08-03 00:14:21 -0300

    [NEW] Generate mix data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movements: ["F", "F2", "F'", "U", "U2", "U'", "R", "R2", "R'", "L", "L2", "L'", "B", "B2", "B'", "D", "D2", "D'"],
+            mixFinal: ""
+        }
+    }
+
+    movDraw() {
+        const sizeMix = Math.floor(Math.random() * (20 - 10 + 1) + 10);
+        const mix = [];
+        let stringMoves = "";
+        for(let i = 0; i < sizeMix; i++) {
+            let movement = Math.floor(Math.random() * 18);
+            mix[i] = this.state.movements[movement];
+            stringMoves += " " + mix[i];
+        }
+        return this.setState({
+            mixFinal: stringMoves
+        });
+    }
+
     render () {
         return (
             <div>
-                <p>{/* TODO Array com as posições e misturas */}</p>
-                <button onClick={() => this.mixCube()}>Mix</button>
+                <p>{this.state.mixFinal}</p>
+                <button onClick={() => this.movDraw()}>MIX</button>
             </div>
         );
     }
