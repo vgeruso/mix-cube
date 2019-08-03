9f76ac76e349d2531cf5a37104902f1d43c7d0cc
VictorGeruso <victor.geruso@gmail.com>
2019-08-03 05:06:23 -0300

    [UPD] App Separation into Components

--- src/index.js
+++ src/index.js
@@ -1,56 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
-import './css/index.css';
-
-class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            movements: ["F", "F2", "F'", "U", "U2", "U'", "R", "R2", "R'", "L", "L2", "L'", "B", "B2", "B'", "D", "D2", "D'"],
-            mixFinal: ""
-        }
-    }
-
-    movDraw() {
-        const sizeMix = Math.floor(Math.random() * (25 - 15 + 1) + 15);
-        const mix = [];
-        let stringMoves = "";
-        for(let i = 0; i < sizeMix; i++) {
-            let movement = Math.floor(Math.random() * 18);
-            mix[i] = this.state.movements[movement];
-            stringMoves += " " + mix[i];
-        }
-        return this.setState({
-            mixFinal: stringMoves
-        });
-    }
-
-    render () {
-        const mix = () => {
-            if(this.state.mixFinal === "") {
-                return (
-                    <p className="p-mod">"Press 'Mix' to draw the mix sequence"</p>
-                );
-            } else {
-                return (
-                    <p>{this.state.mixFinal}</p>
-                );
-            }
-        };
-
-        return (
-            <div className="card" id="cardMix">
-                <div className="card-body">
-                    <div>{mix()}</div>
-                    <button type="button" className="btn btn-primary btn-lg btn-block" onClick={() => this.movDraw()}>Mix</button>
-                </div>
-            </div>
-        );
-    }
-}
 
 ReactDOM.render(
-    <Board />,
+    <App />,
     document.getElementById('root')
 );
